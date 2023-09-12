e15246ebac0ad093cbd82c2e577a8b3569c5f10c
André Pereira Russo <andreprusso7@gmail.com>
2023-09-11 21:32:39 -0300

    Final da Aula

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+let element = document.getElementById("teste");
+console.log(element);
+
+/*
+let nome = prompt("Digite seu nome:");
+let n1 = parseInt(prompt("Digite a nota 1:"));
+let n2 = parseInt(prompt("Digite a nota 2:"));
+let m = (n1 + n2) / 2;
+let status;
+
+
+if (m >= 6) {
+    status = "Aprovado";
+} else if (m < 6) {
+    status = "Reprovado";
+} else { let status = "DIGITE VALORES VALIDOS" }
+
+console.log(`${nome} suas notas são: ${n1} e ${n2} e sua média é ${m} você foi ${status}`);
+*/
